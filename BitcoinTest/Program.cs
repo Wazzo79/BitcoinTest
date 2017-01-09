3b63a860cee8e1abef3628b6fb1c8a80b9752c32
Warren Schwartz <Warren.schwartz@outlook.com>
2017-01-09 11:37:53 +0200

    Add project files.

---
+++ BitcoinTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using NBitcoin;
+using System;
+
+namespace BitcoinTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Key privateKey = new Key();
+            BitcoinSecret mainNetPrivateKey = privateKey.GetBitcoinSecret(Network.Main);
+            Console.WriteLine(mainNetPrivateKey);
+            var address = privateKey.PubKey.GetAddress(Network.Main);
+            Console.WriteLine(address.ToString());
+        }
+    }
+}
---
+++ QueryTransaction/Program.cs
@@ -0,0 +1,16 @@
+﻿using NBitcoin;
+using QBitNinja.Client;
+using System;
+
+namespace QueryTransaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new QBitNinjaClient(Network.Main);
+            var transactionId = uint256.Parse("27226ac6ceea08e2dc8f3a985ed025c1d2a849fe384ae6f8ac8235e0c769c719");
+
+        }
+    }
+}
